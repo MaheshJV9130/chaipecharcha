632795d10edcdbbe4abd8d742af5fc71025e80cc
MaheshJV9130 <maheshvispute082@gmail.com>
2025-07-09 19:48:09 +0530

    Analytics Configured

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -29,6 +31,8 @@ export default function RootLayout({ children }) {
           <Navbar />
           {children}
           <Footer />
+          <Analytics/>
+          <SpeedInsights/>
         </SessionWrapper>
       </body>
     </html>
